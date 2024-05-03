d4b136505c1b347969846da7be16d9ffa23f2f70
Flavio Monteiro <flaviohnm@gmail.com>
2024-05-02 23:58:45 -0300

    feat: add TTL attribute in costumerTable

--- localstack/create_dataproviders.sh
+++ localstack/create_dataproviders.sh
@@ -13,10 +13,20 @@ putItems_dynamodb(){
   echo " 3 costumers add in costumerTable successfully"
 }
 
+update_dynamodb(){
+  awslocal dynamodb update-time-to-live --table-name costumer \
+    --time-to-live-specification "Enable=true, AttributeName=expiration_date"
+  echo "update dynamodb with ttl attribute successfully"
+}
+
 echo "Criando DynamoDB"
 echo "==================="
 create_dynamodb
 
 echo "Adding 3 Costumers in Costumer Table"
+echo "==================="
+putItems_dynamodb
+
+echo "Adding TTL attribute in Costumer Table"
 echo "==================="
 putItems_dynamodb
