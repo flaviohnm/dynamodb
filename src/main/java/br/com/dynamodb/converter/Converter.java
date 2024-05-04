3e4490e8e322a1c98dfe295879ae794f2278f6b3
Flavio Monteiro <flaviohnm@gmail.com>
2024-05-04 01:08:04 -0300

    feat: create new converter methods

--- src/main/java/br/com/dynamodb/converter/Converter.java
+++ src/main/java/br/com/dynamodb/converter/Converter.java
@@ -1,13 +1,56 @@
 package br.com.dynamodb.converter;
 
+import br.com.dynamodb.dto.CostumerDTO;
+import br.com.dynamodb.model.Costumer;
+
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class Converter {
 
-    public String toEpocDate(String date){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+
+    public Long toEpocDate(String date) {
         var convertedDate = LocalDateTime.parse(date);
-        Long expiredAt = convertedDate.plusMonths(3).toEpochSecond(ZoneOffset.ofHours(-3));
-        return expiredAt.toString();
+        return convertedDate.plusMonths(3).toEpochSecond(ZoneOffset.of("America/Sao_Paulo"));
+    }
+
+    public String toStringDate(Long epocDate) {
+        var localDate = Instant.ofEpochSecond(epocDate).atZone(ZoneId.of("America/Sao_Paulo")).toLocalDateTime();
+        return localDate.format(formatter);
+    }
+
+    public String toStringLocalDateTime(String stringDate) {
+        var localDateTime = LocalDateTime.parse(stringDate);
+        return localDateTime.format(formatter);
+    }
+
+    public Costumer toCostumer(CostumerDTO costumerDTO) {
+        var costumer = new Costumer();
+
+        costumer.setCompanyName(costumerDTO.getCompanyName());
+        costumer.setCompanyDocumentNumber(costumerDTO.getCompanyDocumentNumber());
+        costumer.setPhoneNumber(costumerDTO.getPhoneNumber());
+        costumer.setCreateDate(LocalDateTime.now().toString());
+        costumer.setExpirationDate(toEpocDate(costumer.getCreateDate()));
+        costumer.setActive(true);
+
+        return costumer;
+    }
+
+    public CostumerDTO toCostumerDTO(Costumer costumer) {
+        var costumerDTO = new CostumerDTO();
+
+        costumerDTO.setCompanyName(costumer.getCompanyName());
+        costumerDTO.setCompanyDocumentNumber(costumer.getCompanyDocumentNumber());
+        costumerDTO.setPhoneNumber(costumer.getPhoneNumber());
+        costumerDTO.setCreateDate(toStringLocalDateTime(costumer.getCreateDate()));
+        costumerDTO.setExpirationDate(toStringDate(costumer.getExpirationDate()));
+        costumerDTO.setActive(costumer.getActive());
+
+        return costumerDTO;
     }
 }
