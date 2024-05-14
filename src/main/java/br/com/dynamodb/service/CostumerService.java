37e7db36cddfbfad447671d7a20a23cb5367478e
Flavio Monteiro <flaviohnm@gmail.com>
2024-05-14 16:02:02 -0300

    feat: adjust blank space

--- src/main/java/br/com/dynamodb/service/CostumerService.java
+++ src/main/java/br/com/dynamodb/service/CostumerService.java
@@ -1,14 +1,17 @@
 package br.com.dynamodb.service;
 
 import br.com.dynamodb.dto.CostumerDTO;
-import br.com.dynamodb.model.Costumer;
 
 import java.util.List;
 
 public interface CostumerService {
     CostumerDTO saveCostumer(CostumerDTO costumerDTO);
+
     List<CostumerDTO> findAllCostumers();
+
     List<CostumerDTO> findByCompanyName(String companyName);
+
     CostumerDTO updateCostumer(CostumerDTO costumerDTO);
+
     CostumerDTO disableCostumer(String companyDocumentNumber);
 }
