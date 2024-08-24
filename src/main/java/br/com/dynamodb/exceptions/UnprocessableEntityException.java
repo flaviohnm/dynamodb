dd2058ed366cdca544c846be4246a6a4c3ed3cb0
Flavio Monteiro <flaviohnm@gmail.com>
2024-08-24 12:06:05 -0300

    :rocket: refactor: remove blank space

--- src/main/java/br/com/dynamodb/exceptions/UnprocessableEntityException.java
+++ src/main/java/br/com/dynamodb/exceptions/UnprocessableEntityException.java
@@ -4,11 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-public class UnprocessableEntityException extends RuntimeException{
+public class UnprocessableEntityException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public UnprocessableEntityException(String ex) {
-		super(ex);
-	}
+    public UnprocessableEntityException(String ex) {
+        super(ex);
+    }
 }
